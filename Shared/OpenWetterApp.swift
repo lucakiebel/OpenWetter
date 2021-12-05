a9b327183c596369216037f8102331e1cf562065
lucakiebel <lucakiebel@xenoncloud.net>
2021-12-05 14:25:19 +0100

    Initial Commit

---
+++ Shared/OpenWetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWetterApp.swift
+//  Shared
+//
+//  Created by Luca Kiebel on 05.12.21.
+//
+
+import SwiftUI
+
+@main
+struct OpenWetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
